ad56563e5b8457495c180a76bc4882be11e6d062
Koren Minchev <korenminchev@gmail.com>
2023-10-08 00:02:31 +0300

    Fixed phone issues.

--- app/listings/listingCard.tsx
+++ app/listings/listingCard.tsx
@@ -10,18 +10,15 @@ type ListingCardProps = {
 
 export const ListingCard: React.FC<ListingCardProps> = ({ listing }) => {
     return (
-        <Box w={"100%"} borderWidth="1px" borderRadius="lg" overflow="hidden" p={4} mb={4} h="150px">
+        <Box w={"100%"} borderWidth="1px" borderRadius="lg" overflow="hidden" p={4} mb={4} h="175px">
             <Flex direction="row">
                 <Box flex="2" pb="4">
                     <Text fontWeight="semibold" mb={2}>
                         מארח: {listing.name}
                     </Text>
                     <Text fontWeight="semibold">{listing.city}</Text>
-                    <Text mb={1}>קיבולת: {listing.capacity}</Text>
+                    <Text mb={1}>מס׳ אנשים: {listing.capacity}</Text>
                     <Text mb={1}>תיאור: {listing.description || 'אין תיאור'}</Text>
-                </Box>
-                <Spacer />
-                <Box flex="1" pl={5}>
                     <Flex align="center" mb={2}>
                         <Icon as={PhoneIcon} boxSize={5} />
                         <Link href={`tel:${listing.phone}`} ml={2}>{listing.phone}</Link>
