ae8e6332bf43b51efc8988a8ef9e242b7c3e1fd7
Koren Minchev <korenminchev@gmail.com>
2023-10-09 15:08:06 +0300

    Added capacity filter.

---
+++ components/capacity_filter.tsx
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+import { Box, Button, Text, HStack } from '@chakra-ui/react';
+
+interface CapacityFilterProps {
+    capacity: number;
+    onFilterChange: (capacity: number) => void;
+}
+
+const CapacityFilter: React.FC<CapacityFilterProps> = ({ capacity, onFilterChange }) => {
+
+
+  const increaseCapacity = (prev: number) => {
+    if (prev >= 10) return;
+    const newCapacity = prev + 1;
+    onFilterChange(newCapacity);
+  }
+
+  const decreaseCapacity = (prev: number) => {
+    if (prev <= 1) return; 
+    const newCapacity = prev - 1;
+    onFilterChange(newCapacity);
+    
+  }
+
+    return (
+        <Box>
+            <Text mb={2} textAlign={'center'}>קיבולת מינימלית:</Text>
+            <HStack>
+                <Button onClick={(e) => decreaseCapacity(capacity)} size="sm" isDisabled={capacity === 1}>
+                    -
+                </Button>
+                <Text>{capacity >= 10 ? "10+" : capacity}</Text>
+                <Button onClick={(e) => increaseCapacity(capacity)} size="sm" isDisabled={capacity >= 10}>
+                    +
+                </Button>
+            </HStack>
+        </Box>
+    );
+};
+
+export default CapacityFilter;
